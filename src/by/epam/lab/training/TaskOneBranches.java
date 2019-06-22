26dca5be0187fa152e58c3ce6dc51f907f62a150
WESt <WESt@domaim.com>
2019-06-23 00:10:28 +0300

    empty TaskOneBranches class added.

---
+++ src/by/epam/lab/training/TaskOneBranches.java
@@ -0,0 +1,16 @@
+package by.epam.lab.training;
+
+public class TaskOneBranches {
+    /*
+    Basics_of_software_code_development.
+
+    1. Даны два угла треугольника (в градусах).
+    Определить, существует ли такой треугольник, и если да,
+    то будет ли он прямоугольным.
+    */
+
+    public static void main(String[] args) {
+
+    }
+
+}
