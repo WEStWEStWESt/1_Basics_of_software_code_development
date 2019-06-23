8d383d2906894eebec1edcdbb795d79db0fe1243
WESt <WESt@domaim.com>
2019-06-23 13:46:14 +0300

    BranchesTaskOne completed.

--- src/by/epam/lab/training/BranchesTaskOne.java
+++ src/by/epam/lab/training/BranchesTaskOne.java
@@ -1,5 +1,7 @@
 package by.epam.lab.training;
 
+import java.util.Scanner;
+
 public class BranchesTaskOne {
     /*
     Basics_of_software_code_development.
@@ -11,6 +13,23 @@ public class BranchesTaskOne {
 
     public static void main(String[] args) {
 
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Input angel A: ");
+            double angleA = scanner.nextDouble();
+            System.out.print("Input angel B: ");
+            double angleB = scanner.nextDouble();
+
+            if (angleA > 0 && angleB > 0 && (angleA + angleB) < 180) {
+                System.out.println("The triangle exists.");
+                if (angleA == 90 || angleB == 90 || (angleA + angleB) == 90) {
+                    System.out.println("The triangle is rectangular.");
+                } else {
+                    System.out.println("The triangle isn't rectangular!");
+                }
+            } else {
+                System.out.println("The triangle doesn't exist!");
+            }
+        }
     }
 
 }
