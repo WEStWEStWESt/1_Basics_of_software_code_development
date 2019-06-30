df046d38223d5f87499bafdaac481c131e96494e
WESt <WESt@domaim.com>
2019-06-30 16:26:27 +0300

    CyclesTaskOne completed.

--- src/by/epam/lab/training/CyclesTaskOne.java
+++ src/by/epam/lab/training/CyclesTaskOne.java
@@ -8,16 +8,23 @@ public class CyclesTaskOne {
     /*
     Basics_of_software_code_development
 
-    1. Напишите программу, где пользователь вводит любое целое положительное число.
-    А программа суммирует все числа от 1 до введенного пользователем числа.*/
+    1. Напишите программу, где пользователь вводит любое целое положительное число,
+       а программа суммирует все числа от 1 до введенного пользователем числа.*/
 
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
-
             System.out.print("Input a positive integer number: ");
             int number = parseInt(scanner.nextLine());
             int result = 0;
 
+            if (number > 0) {
+                for (int i = 0; i <= number; i++) {
+                    result = result + i;
+                }
+                System.out.println(result);
+            } else {
+                System.out.println("Incorrect number! The program will be closed.");
+            }
         }
     }
 
