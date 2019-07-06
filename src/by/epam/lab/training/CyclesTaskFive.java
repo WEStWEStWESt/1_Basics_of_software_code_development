3d786e1690d59eadd7b3fcbfdabc1cabd557294e
WESt <WESt@domaim.com>
2019-07-06 22:55:46 +0300

    Empty CyclesTaskFive class added.

---
+++ src/by/epam/lab/training/CyclesTaskFive.java
@@ -0,0 +1,16 @@
+package by.epam.lab.training;
+
+public class CyclesTaskFive {
+
+    /*
+      Basics_of_software_code_development
+
+     5. Даны числовой ряд и некоторое число е. Найти сумму тех членов ряда, модуль которых больше или равен
+     заданному е. Общий член ряда имеет вид: An = (1/2^n) + (1/3^n)
+     */
+
+    public static void main(String[] args) {
+
+    }
+
+}
